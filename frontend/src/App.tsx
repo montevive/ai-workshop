4fd1c3a70005ffce068f8df00d9168af7ad24ce7
José Manuel Robles (chema) <jose.robles@aptopayments.com>
2024-11-20 15:26:01 +0100

    chore: frontend - react working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+// import { MainLayout } from './layouts/MainLayout';
+// import { ExperiencesPage } from './pages/ExperiencesPage';
+
+// Import other pages as you create them
+// import { HomePage } from './pages/HomePage';
+// import { ProfilePage } from './pages/ProfilePage';
+// import { BookingsPage } from './pages/BookingsPage';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <CssBaseline />
+      <h1>hola</h1>
+      
+    </>
+  );
+};
+
+export default App;
